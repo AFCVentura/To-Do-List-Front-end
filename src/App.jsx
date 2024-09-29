87fa90f3815a8e74631049f596acaaa63ee64824
Ventura <ventura.py3@gmail.com>
2024-09-28 21:45:55 -0300

    fix: Adjust basic auth routes and redirecting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import Test from "./pages/Test";
 
 function App() {
+  
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/auth" element={<AuthPage authType="login" />}></Route>
-          <Route path="/auth/login" element={<AuthPage authType="login" />}></Route>
-          <Route path="/auth/register" element={<AuthPage authType="register" />}></Route>
+          <Route path="/test" element={<Test />} />
+          <Route path="/auth" element={<Navigate to="/auth/login" />}></Route>
+          <Route
+            path="/auth/login"
+            element={<AuthPage authType="login" />}
+          ></Route>
+          <Route
+            path="/auth/register"
+            element={<AuthPage authType="register" />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </>
